0f0f836d6de615bb1805e739edba63d2728e6b00
Palermo Rodriguez <nicolas.palermo@kcc.com>
2022-04-20 15:52:57 -0300

    7) Login de usuario - Validamos que sean correcto en la ruta auth/login el email y las password
    7) Login de usuario - Validamos que sean correcto en la ruta auth/login el email y las password a través de bcrypts (password) y el email (Por Mongoose)

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -1,17 +1,53 @@
 /////////////////////////////////////////////////////////////
 // Importaciones
 
+const bcrypt = require("bcryptjs/dist/bcrypt");
+const { Usuarios } = require("../models/usuario");
+
+
+
 
 /////////////////////////////////////////////////////////////
 // Funciones del Authentication
 
 
 //POST /api/auth/login
-const login = (req,res) => {
+const login = async (req,res) => {
 
-    res.json({
-        "msg": "POST /api/auth/logi"
-    })
+   try {
+
+        console.log("POST /api/auth/login")
+
+        const {correo, password} = req.body;
+
+        const usuario = await Usuarios.findOne({correo});
+
+
+        //Email
+
+        if (!usuario)
+            return res.status(400).json({
+                msg: "User/Password are invalid. Please try again (Email)"
+        })
+
+        //Password
+        const validPassword = bcrypt.compareSync(password, usuario.password);
+
+        if(!validPassword)
+            return res.status(400).json({
+                msg: "User/Password are invalid. Please try again (Password)"
+        })
+
+
+        res.json({
+           "msg": "POST /api/auth/logi"
+        })
+       
+   } catch (error) {
+        res.status(500).json({
+            "msg": error
+        })
+   }
 
 }
 
