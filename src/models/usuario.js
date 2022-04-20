a66cbcbfd4c0d38bc67cee60ae19e31705ef937f
Palermo Rodriguez <nicolas.palermo@kcc.com>
2022-04-20 15:37:37 -0300

    6) Crear ruta autenticación - Auth/Login
    6) Crear ruta autenticación - Auth - Login. Se creo la ruta de autenticacióm como además se agrego con express-validator  algunos middlewares de valicación para el correo y contraseña como también se creo el controlador/función para la funcionabilidad de esa ruta

---
+++ src/models/usuario.js
@@ -0,0 +1,59 @@
+/////////////////////////////////////////////////////////////
+// Importaciones
+
+
+const { Schema, model } = require('mongoose');
+
+
+/////////////////////////////////////////////////////////////
+// Funciones
+
+const UsuarioSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    correo: {
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatorio']
+    },
+    img: {
+        type: String
+    },
+    rol: {
+        type: String,
+        required: true,
+        enum: ['ADMIN_ROLE', 'USER_ROLE']
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+
+})
+
+//Sobreescribimo el JSON de MongoDB del Schema
+//Para que solo retornemos los campos necesarios
+UsuarioSchema.methods.toJSON = function() {
+    const { __v, password, ...user } = this.toObject();
+    return user
+}
+
+const Usuarios = model('Usuario', UsuarioSchema);
+
+
+/////////////////////////////////////////////////////////////
+// Exportamos
+
+module.exports = {
+    Usuarios
+}
